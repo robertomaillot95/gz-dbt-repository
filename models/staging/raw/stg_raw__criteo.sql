6ebb14d156b0baae8dd1274be564c99ed5da82dd
maillotroberto95@gmail.com <maillotroberto95@gmail.com>
2024-02-07 11:32:42 +0000

    UNION ALL

--- models/staging/raw/stg_raw__criteo.sql
+++ models/staging/raw/stg_raw__criteo.sql
@@ -12,7 +12,7 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name as campaing_name,
+        campgn_name as campaign_name,
         CAST(ads_cost AS FLOAT64) AS ads_cost,
         impression,
         click
