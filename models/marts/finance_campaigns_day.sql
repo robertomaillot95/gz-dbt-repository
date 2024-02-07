bea3d1c3475a87820fe4670a46f69f30e5570f77
maillotroberto95@gmail.com <maillotroberto95@gmail.com>
2024-02-07 13:17:00 +0000

    finance campaigns day

--- models/marts/finance_campaigns_day.sql
+++ models/marts/finance_campaigns_day.sql
@@ -1,6 +1,14 @@
 SELECT 
-    MAX(operational_margin) - sum(ads_cost) AS Ads_margin
+    date_date
+    ,operational_margin - ads_cost AS ads_margin
+    ,average_basket
+    ,operational_margin
+    ,revenue
+    ,margin
+    ,ads_cost
+    ,ads_clicks
+    ,ads_impression
 FROM {{ ref('finance_days') }}
-JOIN {{ ref('int_campaigns') }} USING(date_date)
-GROUP BY date_date
+JOIN {{ ref('int_campaigns_day') }} USING(date_date)
+order by date_date DESC
 
