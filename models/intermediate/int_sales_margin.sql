4c490d55110950aaa6eb508879be344db00d23de
maillotroberto95@gmail.com <maillotroberto95@gmail.com>
2024-02-07 15:25:38 +0000

    macro

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,12 @@
+WITH a AS (
 SELECT 
-    quantity*purchase_price AS purchase_cost
+    *
+    ,quantity*purchase_price AS purchase_cost
     ,ROUND(revenue - (quantity*purchase_price),2) AS margin
 FROM {{ ref('stg_raw__product') }}
 JOIN {{ ref('stg_raw__sales') }} USING(products_id)
+)
+SELECT
+    *
+    ,{{ margin_percent('revenue','purchase_cost') }} as margin_percent
+FROM a
